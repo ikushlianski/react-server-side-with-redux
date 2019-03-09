cc25713f94256f565906c59ca9932c51ee3fcd0d
Ilya Kushlianski <kushliansky@gmail.com>
2019-03-09 17:35:09 +0300

    Add Providers to server and client

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,20 @@
 // entrypoint for client codebase
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.hydrate(<Home/>, document.getElementById("app"));
+import Routes from './Routes';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+ReactDOM.hydrate(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById("app")
+);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send(renderer());
+app.get('*', (req, res) => {
+  const store = createStore();
+
+  // TODO init and load data into store
+
+  res.send(renderer(req, store));
 });
 
-app.listen(3000, () => console.warn('listening to 3000'));
+app.listen(3000, () => console.warn(`[${new Date()}] listening to 3000`));
